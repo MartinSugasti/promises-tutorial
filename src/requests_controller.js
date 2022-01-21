8ce5cb044362d2d54cc0d6de04206027cf1c38a7
MartinSugasti <martinsugasti@gmail.com>
2022-01-21 11:19:21 +0100

    Use async and await intead of then

--- src/requests_controller.js
+++ src/requests_controller.js
@@ -1,14 +1,17 @@
 const requestsController = (() => {
-  function getGiphURLByKeyword(keyword) {
-    return fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=ed73ctHCkXxVaoWQqT62B7p5ujurNwkF&s=${keyword}`,
-      {
-        mode: 'cors',
-      }
-    )
-      .then((response) => response.json())
-      .then((body) => body.data.images.original.url)
-      .catch(() => 'https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif');
+  async function getGiphURLByKeyword(keyword) {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=ed73ctHCkXxVaoWQqT62B7p5ujurNwkF&s=${keyword}`,
+        {
+          mode: 'cors',
+        }
+      );
+      const body = await response.json();
+      return body.data.images.original.url;
+    } catch (error) {
+      return 'https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif';
+    }
   }
 
   return { getGiphURLByKeyword };
